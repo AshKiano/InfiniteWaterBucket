60d57c04c5fe7987e9b9b4e5799972e7dd1d9b06
Josef Vyskoèil <josef.vyskocil@cleverlance.com>
2024-05-29 11:30:04 +0200

    version 1.6

--- src/main/java/com/ashkiano/infinitewaterbucket/InfiniteWaterBucket.java
+++ src/main/java/com/ashkiano/infinitewaterbucket/InfiniteWaterBucket.java
@@ -6,12 +6,13 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerBucketEmptyEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.java.JavaPlugin;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
@@ -21,7 +22,6 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
-//TODO pridat permisi
 public class InfiniteWaterBucket extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
@@ -93,6 +93,18 @@ public class InfiniteWaterBucket extends JavaPlugin implements Listener {
 
                     player.getInventory().setItemInMainHand(infiniteBucket);
                 }, 1L);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onPlayerDropItem(PlayerDropItemEvent event) {
+        ItemStack droppedItem = event.getItemDrop().getItemStack();
+        if (droppedItem.getType() == Material.BUCKET && droppedItem.hasItemMeta() && droppedItem.getItemMeta().hasLore()) {
+            List<String> lore = droppedItem.getItemMeta().getLore();
+            if (lore != null && lore.contains("Infinite Bucket")) {
+                event.setCancelled(true);
+                event.getPlayer().sendMessage("You cannot drop the infinite water bucket!");
             }
         }
     }
